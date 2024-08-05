e7b8c5351b49e3713974307a30f7c76e14a712e6
yunishello <yunishello@gmail.com>
2024-08-05 11:20:46 +0100

    creating a react template with vite and tailwincss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <h1
+        className="text-3xl text-center text-blue-700"
+      >Quick template to integrate react + vite + tailwindcss</h1>
+    </>
+  )
+}
+
+export default App
